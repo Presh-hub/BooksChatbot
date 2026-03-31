b43f358b4d311538bb499e1f068c29aa8912ffcc
Presh-hub <ijeomaprecious698@gmail.com>
2026-03-31 21:27:53 +0800

    Added full books.py

--- books.py
+++ books.py
@@ -1,30 +1,68 @@
 import pandas as pd
+import json
+
+# SWITCH DATASET 
+MODE = "manual"   # change to "kaggle" if needed
+
+
+
+# LOAD DATA
+
 
 def load_books():
-    df = pd.read_csv(
-        "data/books.csv",
-        encoding="latin-1",
-        sep=";",
-        quotechar='"',
-        on_bad_lines='skip',
-        low_memory=False
-    )
+    if MODE == "kaggle":
+        df = pd.read_csv(
+            "data/books.csv",
+            encoding="latin-1",
+            sep=";",
+            quotechar='"',
+            on_bad_lines='skip',
+            low_memory=False
+        )
 
-    df = df[["Book-Title", "Book-Author"]].dropna().drop_duplicates()
+        df = df[["Book-Title", "Book-Author"]].dropna().drop_duplicates()
 
-    levels = ["Beginner", "Intermediate", "Advanced"]
+        levels = ["Beginner", "Intermediate", "Advanced"]
+        df["Level"] = [levels[i % 3] for i in range(len(df))]
 
-    df["Level"] = [levels[i % 3] for i in range(len(df))]
-    return df.head(20)
+        return df.head(20)
+
+    else:
+        with open("data/books.json", "r") as file:
+            return json.load(file)
+
+
+
+# LIST BOOKS
+
 
 def list_books():
-    df = load_books()
+    data = load_books()
 
     result = []
-    for i in range(len(df)):
-        result.append(f"{i+1}. {df.iloc[i]['Book-Title']} by {df.iloc[i]['Book-Author']}")
+
+    if MODE == "kaggle":
+        for i in range(len(data)):
+            result.append(f"{i+1}. {data.iloc[i]['Book-Title']} by {data.iloc[i]['Book-Author']}")
+
+    else:
+        for b in data:
+            result.append(f"{b['id']}. {b['title']} by {b['author']}")
 
     return result
 
-    
-    
+
+
+# GET SUMMARY (ONLY JSON)
+
+def get_book_summary(user_input):
+    if MODE == "kaggle":
+        return "❌ sorry no available summaries."
+
+    books = load_books()
+
+    for b in books:
+        if b["title"].lower() in user_input.lower():
+            return f"{b['title']}:\n{b['summary']}"
+
+    return "Book not found."
